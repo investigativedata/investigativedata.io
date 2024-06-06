3851923021e42c16bd5c62c1a9bc99457058fb03
Simon Wörpel <simon.woerpel@pm.me>
2024-06-06 06:57:05 +0200

    👷 Make next output configurable via env var

--- config.ts
+++ config.ts
@@ -6,9 +6,10 @@ export const DIRECTUS_DRAFT_TOKEN =
   process.env.DIRECTUS_DRAFT_TOKEN || "secret-draft-token";
 export const DIRECTUS_API_TOKEN = process.env.DIRECTUS_API_TOKEN || "";
 
-export const PREVIEW = (process.env.PREVIEW || "true") === "true";
+export const PREVIEW = (process.env.PREVIEW || "1") === "1";
 
-const status_filter = PREVIEW ? {} : { page: { status: { _eq: "published" } } };
+// const status_filter = PREVIEW ? {} : { page: { status: { _eq: "published" } } };
+const status_filter = PREVIEW ? {} : {};
 
 export const DIRECTUS_DEFAULT_PAGE_FILTER = {
   ...status_filter,
