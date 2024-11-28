17b40c9f2d98b6fb60d7f6e3b4bf5d86ad21ee2d
Simon Wörpel <simon.woerpel@pm.me>
2024-11-28 19:51:51 +0100

    💄 (components) Improve NewsletterForm

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -1,24 +1,40 @@
+import { useContext } from "react";
 import { INewsletterItem } from "@/lib/types";
 import Button from "@mui/joy/Button";
 import Input from "@mui/joy/Input";
 import Stack from "@mui/joy/Stack";
+import { BACKGROUNDS, BLACK, PageContext } from "@investigativedata/style";
 
 export default function NewsletterForm({
   action,
   listUid,
 }: React.PropsWithChildren<INewsletterItem>) {
+  const { currentColor } = useContext(PageContext);
   return (
     <form action={action} method="post">
       <Stack
         spacing={1}
         direction="row"
-        justifyContent="space-between"
+        // justifyContent="space-between"
         alignSelf="center"
         sx={{ w: "100%" }}
       >
         <Input placeholder="email@example.org" required name="email" />
         <input type="hidden" name="l" value={listUid} />
-        <Button color="success" type="submit">
+        <Button
+          color="success"
+          type="submit"
+          sx={{
+            color: BACKGROUNDS[currentColor],
+            backgroundColor: BLACK,
+            "&:hover": {
+              backgroundColor: BLACK,
+            },
+            "&:active": (theme) => ({
+              backgroundColor: theme.colorSchemes.dark.palette.text,
+            }),
+          }}
+        >
           Subscribe
         </Button>
       </Stack>
