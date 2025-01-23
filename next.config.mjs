a208752a8d78c970c952572fc1ec983d0b7c6ee4
Simon Wörpel <simon.woerpel@pm.me>
2025-01-23 17:14:14 +0100

    ⬆️ All the things

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
-const withMDX = require("@next/mdx")();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.EXPORT === "1" ? "export" : "standalone",
   trailingSlash: true,
   transpilePackages: ["next-mdx-remote"], // FIXME: https://github.com/hashicorp/next-mdx-remote/issues/381
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
   images: {
     loader: "custom",
     loaderFile: "./image-loader.ts",
@@ -23,4 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withMDX(nextConfig);
+export default nextConfig;
